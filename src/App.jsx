8a40146ea4ab45dc82fbc89d3117d3312f8f5ff6
Sudhanshu <156512335+ixsudhanshu27@users.noreply.github.com>
2024-03-13 20:44:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { useState } from 'react'
 import Navbar from './Navbar.jsx'
-import Projectcards from './Projectcards.jsx'
-import Projectdata from './Projectdata.jsx'
 import Home from './Home.jsx'
 import Contact from './Contacts.jsx'
 import Footer from './footer.jsx'
 import './App.css'
 
 function App() {
-  const DataArray = Projectdata.data.projects
-   
-  const ProjectCards = DataArray.map(projects => <Projectcards key={projects.id} name = {projects.name} about = {projects.about} />)
 
   return (
     <div className="App">
