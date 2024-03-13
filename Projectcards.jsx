bdac4dd53eb21cb58b7ceab3e8dbc15ddc245077
Sudhanshu <156512335+ixsudhanshu27@users.noreply.github.com>
2024-03-13 20:43:11 +0530

    Delete Projectcards.jsx

--- Projectcards.jsx
+++
@@ -1,8 +0,0 @@
-export default function Projectcards ({name, about}){
-    return (
-        <div className="container">
-             <h3>{name}</h3>
-             <h3>{about}</h3>
-        </div>
-    )
-}
